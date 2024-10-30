42f20b6ef4877759f0a8264a3ddb45f7552ca00a
morganm562 <morganm3813@gmail.com>
2024-10-30 09:43:30 -0400

    Update RecipeRepository.java

--- RecipeRepository.java
+++ RecipeRepository.java
@@ -1,11 +1,10 @@
 package main.java.com.example.demo;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-@Repository
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
     List<Recipe> findByNameContainingIgnoreCase(String name);
-
     List<Recipe> findByIngredientsContainingIgnoreCase(String ingredient);
 }
