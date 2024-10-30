cea3f695d673be60426d5bcfd9b10ccf28040412
morganm562 <morganm3813@gmail.com>
2024-10-30 09:45:20 -0400

    Update RecipeRepository.java

--- RecipeRepository.java
+++ RecipeRepository.java
@@ -1,10 +1,33 @@
-package main.java.com.example.demo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.ArrayList;
 import java.util.List;
 
-public interface RecipeRepository extends JpaRepository<Recipe, Long> {
-    List<Recipe> findByNameContainingIgnoreCase(String name);
-    List<Recipe> findByIngredientsContainingIgnoreCase(String ingredient);
+public class RecipeRepository {
+    private List<Recipe> recipes = new ArrayList<>();
+    private Long currentId = 1L;
+
+    public Recipe save(Recipe recipe) {
+        recipe.setId(currentId++);
+        recipes.add(recipe);
+        return recipe;
+    }
+
+    public List<Recipe> findByNameContainingIgnoreCase(String name) {
+        List<Recipe> result = new ArrayList<>();
+        for (Recipe recipe : recipes) {
+            if (recipe.getName().toLowerCase().contains(name.toLowerCase())) {
+                result.add(recipe);
+            }
+        }
+        return result;
+    }
+
+    public List<Recipe> findByIngredientsContainingIgnoreCase(String ingredient) {
+        List<Recipe> result = new ArrayList<>();
+        for (Recipe recipe : recipes) {
+            if (recipe.getIngredients().toLowerCase().contains(ingredient.toLowerCase())) {
+                result.add(recipe);
+            }
+        }
+        return result;
+    }
 }
