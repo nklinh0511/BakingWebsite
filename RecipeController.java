000739d69f114be750f86b5da95ffa7778b9320a
morganm562 <morganm3813@gmail.com>
2024-10-30 09:43:13 -0400

    Update RecipeController.java

--- RecipeController.java
+++ RecipeController.java
@@ -1,5 +1,10 @@
 package main.java.com.example.demo;
-import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -7,8 +12,12 @@ import java.util.List;
 @RequestMapping("/recipes")
 public class RecipeController {
 
+    private final RecipeService recipeService;
+
     @Autowired
-    private RecipeService recipeService;
+    public RecipeController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
 
     @GetMapping("/searchByName")
     public List<Recipe> searchRecipesByName(@RequestParam String name) {
